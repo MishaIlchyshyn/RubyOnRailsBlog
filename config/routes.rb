4e6e5ef92f9929040f3a9472e46f32fb70be40f5
MishaIlchyshyn <ilchyshynmuchailo@gmail.com>
2020-07-28 16:48:46 +0300

    Install Devise gem and implement authorization user (register, sign-in, logout)

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   
+  devise_for :users
+  get 'persons/profile'
   root 'pages#index'
 
   get 'about', to: 'pages#about'
+
+  get 'persons/profile', as: 'user_root'
 
   resources :posts
 
