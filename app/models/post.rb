9f52b0c4ef569ab93b9e565fa86da71df9049433
MishaIlchyshyn <ilchyshynmuchailo@gmail.com>
2020-08-12 13:04:26 +0300

    Add upload image to create post form

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,6 @@ class Post < ActiveRecord::Base
         validates :title, presence: true, 
             length: {minimum: 3, maximum: 50}
         validates :description, presence: true, 
-            length: {minimum: 10, maximum: 300}
+            length: {minimum: 10, maximum: 2000}
+        mount_uploader :image, ImageUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_29_104342) do
+ActiveRecord::Schema.define(version: 2020_08_11_163551) do
 
   create_table "articles", force: :cascade do |t|
     t.string "test"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2020_07_29_104342) do
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string "image"
   end
 
   create_table "users", force: :cascade do |t|
