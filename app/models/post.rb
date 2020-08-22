6243e1426817998712bc85df6cd0359b95369d8d
MishaIlchyshyn <ilchyshynmuchailo@gmail.com>
2020-08-22 11:28:26 +0300

    Rewrite action text to work version

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,10 @@
 class Post < ActiveRecord::Base
     acts_as_punchable
-    has_rich_text :content
+    has_rich_text :body
     has_many :comments, dependent: :destroy
     validates :title, presence: true, 
         length: {minimum: 3, maximum: 50}
-    validates :description, presence: true, 
+    validates :body, presence: true, 
         length: {minimum: 10, maximum: 2000}
     mount_uploader :image, ImageUploader
 end
