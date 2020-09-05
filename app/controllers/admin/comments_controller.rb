bbe183fbcfec565a223e8eb1a7762f1cc6ff3450
MishaIlchyshyn <ilchyshynmuchailo@gmail.com>
2020-09-05 10:44:34 +0300

    Take out "before_action: authenticate_user!" in BaseController, fixed style to alerts

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,6 +1,4 @@
 class Admin::CommentsController < Admin::BaseController
-    before_action :authenticate_user!
-    
     def index
         @comments = Comment.all
     end
--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,6 +1,4 @@
 class Admin::PostsController < Admin::BaseController
-    before_action :authenticate_user!
-
     def index
         @posts = Post.all
     end
